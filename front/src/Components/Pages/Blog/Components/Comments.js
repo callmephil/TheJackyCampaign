c3d7d44457e3b03b4169459dffa1d27a308cd810
Callmephil <phil19-94@hotmail.fr>
2019-02-24 13:39:32 +0200

    Front/Pages/Home : Added Stateless Components Structure

---
+++ front/src/Components/Pages/Blog/Components/Comments.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CommentsComponent = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default CommentsComponent;
---
+++ front/src/Components/Pages/Home/Components/BlogFeed.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BlogFeedComponent = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default BlogFeedComponent;
