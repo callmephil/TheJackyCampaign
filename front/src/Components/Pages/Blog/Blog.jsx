10652bf9e39eaa609498449a4491473ee233b5f9
Callmephil <phil19-94@hotmail.fr>
2019-02-25 17:45:22 +0200

    Front/Packages/Component : Re-Added Axios, Testing Interaction

--- front/src/Components/Pages/Blog/Blog.jsx
+++ front/src/Components/Pages/Blog/Blog.jsx
@@ -1,7 +1,22 @@
 import React, {Component} from 'react';
+import { BlogServices } from '../../../Services/BlogServices';
 
 class Blog extends Component
 {
+  constructor() {
+    super();
+
+
+    this.blogService = new BlogServices();
+  }
+  // title text DEFAULT '',
+	// content blob DEFAULT '',
+	// date datetime DEFAULT 0,
+	// author_id text DEFAUTL ''
+  componentDidMount() {
+    this.blogService.createPost({title:"This is a title", content:"This is a content", date:'10/20/1994', author_id:'Phil'})
+    .then(x => console.log(x));
+  }
   render(){
     return (
       <div className="container">
