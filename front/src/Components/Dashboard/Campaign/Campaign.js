095d6bb0c778c9f60836ac7e47312398d5909b3c
Callmephil <phil19-94@hotmail.fr>
2019-03-14 16:48:06 +0200

    Front/Components/Campaign : Refractoring Part 1

--- front/src/Components/Dashboard/Campaign/Campaign.js
+++ front/src/Components/Dashboard/Campaign/Campaign.js
@@ -71,33 +71,42 @@ const Pledges = () => {
     return (
             <div className="pledge-modal-content">
                 <div className="pledge-modal-header">
-                    <div className="row">
                         <div className="donate-amount-option">
                             <h4>Choose an amount</h4>
                             <ul className="predefined-amount">
                                 <li>
+                                    <label className="">
+                                        <input type="radio" name="donation-amount" value="10"/>$10
+                                    </label>
+                                </li>
+                                <li>
+                                    <label className="">
+                                        <input type="radio" name="donation-amount" value="20"/>$20
+                                    </label>
+                                </li>  
+                                <li>
+                                    <label className="">
+                                        <input type="radio" name="donation-amount" value="30"/>$30
+                                    </label>
+                                </li>
+                                <li>
                                     <label className="selected">
-                                        <input type="radio" name="donation-amount"/>$10
+                                        <input type="radio" name="donation-amount" value="50"/>$50
                                     </label>
                                 </li>
                                 <li>
-                                    <label>
-                                        <input type="radio" name="donation-amount"/>$20
+                                    <label className="">
+                                        <input type="radio" name="donation-amount" value="100"/>$100
                                     </label>
                                 </li>
                                 <li>
-                                    <label>
-                                        <input type="radio" name="donation-amount"/>$30
+                                    <label className="">
+                                        <input type="radio" name="donation-amount" value="150"/>$150
                                     </label>
                                 </li>
                                 <li>
-                                    <label>
-                                        <input type="radio" name="donation-amount"/>$50
-                                    </label>
-                                </li>
-                                <li>
-                                    <label>
-                                        <input type="radio" name="donation-amount"/>$100
+                                    <label className="">
+                                        <input type="radio" name="donation-amount" value="200"/>$200
                                     </label>
                                 </li>
                             </ul>
@@ -111,7 +120,6 @@ const Pledges = () => {
                         </div>
                     </div>
                 </div>
-            </div>
     );
 }
 
@@ -142,52 +150,81 @@ const TabPanels = () => {
     )
 }
 
+const CampaignTitle = () => {
+    return (
+        <>
+        <h1 className="big-title-centered">First Campaign Title</h1>
+        <div className="spacer-20"/>
+        <p className="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <div className="spacer-20"/>
+        </>
+    );
+}
+
+const ProgressBar = () => {
+    return (
+        <>
+            <span className="label label-default">Campaign progress</span>
+                <div className="progress">
+                    <div style={{width: "88%", animationDelay: "100ms"}} 
+                        className="progress-bar progress-bar-primary" 
+                        data-appear-progress-animation="88%" 
+                        data-appear-animation-delay="100"> 
+                        <span className="progress-bar-tooltip" style={{opacity: "1"}}>88%</span> 
+                    </div>
+                </div>
+                <div className="pull-left">Raised <strong>$176000</strong></div>
+                <div className="pull-right">Goal <strong className="accent-color">$200000</strong></div>
+        </>
+    );
+}
+
+const Statistics = () => {
+    return (
+    <ul className="list-group">
+        <li className="list-group-item">Total Funder<span className="badge">2000</span></li>
+        <li className="list-group-item">Days left to fundraising<span className="badge">10</span></li>
+        <li className="list-group-item">Countries helping<span className="badge">130</span></li>
+    </ul>
+    );
+}
+
 export default class Campaign extends Component {
   render() {
     return (
         <div className="campaign-container content-block">
             {/* HEAD */}
-            <h1 className="big-title-centered">First Campaign Title</h1>
-            <div className="spacer-20"/>
-            <p className="lead">Nepal has been seriously devastated by the recent earthquake. Over 8,000 people have died (with death toll rising), thousands more are injured, and countless more have been displaced. Many homes, temples, and public monuments have been destroyed throughout the affected areas.</p>
-            <div className="spacer-20"/>
+            <CampaignTitle />
             {/* PROGRESS & PLEDGES  */}
             <div className="row">
-                <div className="col-md-5 col-sm-5">
-                    <span className="label label-default">Campaign progress</span>
-                    <div className="progress">
-                        <div style={{width: "88%", animationDelay: "100ms"}} 
-                            className="progress-bar progress-bar-primary" 
-                            data-appear-progress-animation="88%" 
-                            data-appear-animation-delay="100"> 
-                            <span className="progress-bar-tooltip" style={{opacity: "1"}}>88%</span> 
-                        </div>
-                    </div>
-                    <div className="pull-left">Raised <strong>$176000</strong></div>
-                    <div className="pull-right">Goal <strong className="accent-color">$200000</strong></div>
-                    <div className="spacer-20"/>
-                    <div className="post-media">
-                        <img alt="" src="http://www.lebanoninapicture.com/Prv/Images/Pages/Page_136076/heaven-on-earth-lebanon-chouwen-lake-trees-heav-9-8-2017-3-01-16-pm-l.jpg" />
-                    </div>
-                </div>
 
-                <div className="col-md-7 col-sm-7">
-                    <ul className="list-group">
-                          <li className="list-group-item">Total Funder<span className="badge">2000</span></li>
-                          <li className="list-group-item">Days left to fundraising<span className="badge">10</span></li>
-                          <li className="list-group-item">Countries helping<span className="badge">130</span></li>
-                      </ul>
-                    <Pledges />
-                    
-                    <div className="spacer-20"/>
-                    {/* <a href="#" className="btn btn-primary btn-block" data-toggle="modal" data-target="#DonateModal">Fund Now</a> */}
-                    <button className="btn btn-primary btn-block">Fund Now</button>
-                </div>
-
+            <div className="col-md-5 col-sm-5">
+                <ProgressBar />
                 <div className="spacer-20"/>
+                <div className="post-media">
+                    <iframe width="560" height="315" 
+                    src="https://www.youtube.com/embed/coIXMyWzpAU" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen>
+                    </iframe>
+
+                    {/* <img alt="" src="http://www.lebanoninapicture.com/Prv/Images/Pages/Page_136076/heaven-on-earth-lebanon-chouwen-lake-trees-heav-9-8-2017-3-01-16-pm-l.jpg" /> */}
+                </div>
+            </div>
+
+            <div className="col-md-7 col-sm-7">
+                <Statistics />
+                <Pledges />
+                <div className="spacer-20"/>
+                <button className="btn btn-primary btn-block">Fund Now</button>
+            </div>
+
+            <div className="spacer-20"/>
             </div>
             {/* TABS  */}
             <TabPanels />
+            <div className="spacer-20"/>
             </div>
     );
   };
